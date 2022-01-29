fb84eeaaeb5703934d1a6b14716a81b914c8ec48
gogobro <94741085+gogobro@users.noreply.github.com>
2022-01-29 20:10:12 +0300

    Add files via upload

---
+++ project2/floor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="floor" tilewidth="18" tileheight="18" tilecount="1444" columns="38">
+ <image source="floor2.png" width="700" height="700"/>
+</tileset>
